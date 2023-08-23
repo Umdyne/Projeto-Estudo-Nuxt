0339547928e960589406fd4de9367faa5195f93f
Umdyne <luansilva2357@gmail.com>
2023-08-23 12:16:12 -0300

    Inicio

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full px-2 bg-black">
+        <div class="w-full p-4 border b-gray-200 flex items-center justify-between">
+            <span class="text-xl font-semibold">Jonh Silva</span>
+            <div class="w-32 space-x-2">
+                <button>Editar</button>
+                <button>Excluir</button>
+            </div>
+        </div>
+    </div>
+    <button class="w-full p-4 mt-8 border b-blue-200 rounded bg-blue-500 text-white flex items-center">
+        Adicionar Contato
+    </button>
+</template>
